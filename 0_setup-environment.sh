5a0a4c59e89287a1e5854e1c9bc066a7989a1c72
JCZD <jczd@engrenage.ch>
2025-07-29 13:27:03 +0200

    Hello world build succeeded, app runs

--- 0_setup-environment.sh
+++ 0_setup-environment.sh
@@ -1,26 +1,22 @@
 #!/usr/sbin/bash
 
+# Required system packages
+#	Gentoo:
+# 		emerge dev-java/openjdk-bin:17
+# 		eselect java-vm list
+# 		eselect java-vm set system $N
+#
+# 		emerge dev-util/ccache
+
 #
 # setup build environment
 #
-
-# emerge --ask dev-java/openjdk:8
-# eselect java-vm list
-# eselect java-vm set system $N
-#
-# emerge --ask dev-util/ccache
-
 python -m venv .build-env
 . .build-env/bin/activate
-pip install cython
-pip install setuptools # python >= 3.12
-pip install buildozer
+pip install `cat build-env.freeze`
 buildozer init
 buildozer update
 # DO NOT INSTALL kivy,jnius
-#mkdir ~/.local/bin
-#ln -s /usr/lib64/openjdk-8/bin/java{,c} ~/.local/bin
-#export PATH="${HOME}/.local/bin:${PATH}"
 
 #
 # setup app environment
--- 1_build-all.sh
+++ 1_build-all.sh
@@ -1,12 +1,20 @@
 #!/usr/sbin/bash
 
+DOMAINE=`grep package.domain buildozer.spec | sed s/\ //g | cut -f 2 -d '='`
+APPNAME=`grep package.name buildozer.spec | sed s/\ //g | cut -f 2 -d '='`
+
 # build for Android
 . .build-env/bin/activate
 export JAVA_HOME=/etc/java-config-2/current-system-vm
 export PATH="$JAVA_HOME/bin:$PATH"
 buildozer android clean
-buildozer -v android debug
+buildozer -v android debug && \
+read -p "${DOMAIN}.${APPNAME} compilation finished ; press [enter] to install"
+adb install `ls -t bin/*.apk | head -1` && \
+echo "${DOMAIN}.${APPNAME} installed ; watching log"
 
+#ch.engrenage.hyperwatch/org.kivy.android.PythonActivity
+adb logcat | grep --color=never ${DOMAIN}.${APPNAME}
 
 # Build for IOs
 # TODO
